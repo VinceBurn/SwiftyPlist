2f3009701cf6be0d04c7d65bb67e690710f8c0e7
Vincent Bernier <vincentbernier@hotmail.com>
2015-08-21 16:43:33 -0400

    Initial Commit

---
+++ PlistParsing/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PlistParsing
+//
+//  Created by Vincent Bernier on 21-08-2015.
+//  Copyright (c) 2015 Vincent Bernier. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
