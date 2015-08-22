1ab9ff536c454437efa621ee2041555fdef7dbe7
Vincent Bernier <vincentbernier@hotmail.com>
2015-08-22 17:29:16 -0400

    add playground to the project for testing purpose

---
+++ _Experiment/MyPlayground.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+struct myStruct {
+}
+
+func switchOnAny(any: Any) -> String {
+    println("Dynamic Type == \(any.dynamicType)")
+    switch any {
+    case let array as [Any]:
+        return "Array"
+    case let array as NSArray:
+        return "NSArray"
+    default:
+        return "Default"
+    }
+}
+
+let emptyStringArray : [String] = []
+let stringArray : [String] = ["Bob", "Roger"]
+let intArray = [1, 2, 3]
+let customStructArray : [myStruct] = []
+
+println("\t\touput : \(switchOnAny([]))")
+println("\t\touput : \(switchOnAny(emptyStringArray))")
+println("\t\touput : \(switchOnAny(stringArray))")
+println("\t\touput : \(switchOnAny(intArray))")
+println("\t\touput : \(switchOnAny(customStructArray))")
+
